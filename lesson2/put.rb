b8f80e735b3dab53981969ff37b9b24ec6dffa18
MisterioSST <misteriotim77@gmail.com>
2018-04-14 11:49:22 +0300

    newcommit6

--- lesson2/put.rb
+++ lesson2/put.rb
@@ -7,5 +7,5 @@ family_name = gets.chomp
 print "What is your age? "
 my_age = gets.to_i
 
-my_str =  "Your name is #{my_name}. Your family name is = #{family_name}. Your age is = #{my_age}."
+my_str =  "Your name is #{my_name}. Your family name is #{family_name}. Your age is #{my_age}."
 puts my_str.reverse
