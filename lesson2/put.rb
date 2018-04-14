8a0a24ee62cdc500963639fff0de8f446bdd5fe8
MisterioSST <misteriotim77@gmail.com>
2018-04-14 10:56:34 +0300

    newcommit5

---
+++ lesson2/put.rb
@@ -0,0 +1,11 @@
+print "Hello, what your name? "
+my_name = gets.chomp
+
+print "What your family name? "
+family_name = gets.chomp
+
+print "What is your age? "
+my_age = gets.to_i
+
+my_str =  "Your name is #{my_name}. Your family name is = #{family_name}. Your age is = #{my_age}."
+puts my_str.reverse
